d82ebe5dd373af6b0be9c4450b57320a038ceb5f
Jagadeesh-Vadlamuri <jagadeesh.jacky1001@gmail.com>
2022-06-03 10:24:56 +0530

    added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
+
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
 
